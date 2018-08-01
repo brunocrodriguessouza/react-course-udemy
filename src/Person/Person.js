9b81d94763d3bb33e82a1664721f786988dc0bb6
Bruno Souza <brunoprogramadorjava@gmail.com>
2018-08-01 18:03:16 -0300

    Section 3 46.Adding Two Way Binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import React, {Component} from 'react';
 
-const person = (props) => {
+const person = ( props ) => {
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 }
